7486ab1c3c0eef2fbdfa170f6253e7c472fd7a7f
Claudio Filho <devclaudiofilho@gmail.com>
2019-11-19 16:10:37 -0300

    first commit

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.zattini.com.br/produtos/tenis-couro-walkabout-masculino/06/H25-1065-006/H25-1065-006_detalhe2.jpg?ims=326x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>tenis doido</strong>
+              <span>R$ 10</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$20</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$100,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
